3be2a204abd10f6627f917474fd3d7b6bc4c9a59
krishnaachyuth <34981932+krishnaachyuth@users.noreply.github.com>
2024-03-09 02:40:07 -0500

    uploading top file

--- topmem-3.sv
+++ topmem-3.sv
@@ -21,71 +21,31 @@ logic ALE;
 logic DTR;
 logic DEN;
 
-logic OE,rw;
-logic ld_Addr,ld_data;
-logic [3:0] cs;
+
+logic [3:0] CS;
 //logic cs;
 genvar i,j;
 
 logic [19:0] Address;
 wire [7:0]  Data;
 
-localparam bit[19:0] MEM_LSB[1:0] = '{20'h8FFFF , 20'h00000}; 
-localparam bit[19:0] MEM_MSB[1:0] = '{20'hFFFFF , 20'h7FFFF};    
+localparam bit[19:0] MEM_LSB[1:0] = '{20'h80000 , 20'h00000};   //524288,0
+localparam bit[19:0] MEM_MSB[1:0] = '{20'hFFFFF , 20'h7FFFF};   //1048575,524287 
 
-localparam bit[16:0] IO_LSB[1:0] = '{16'hFF00 , 16'h1C00};
-localparam bit[16:0] IO_MSB[1:0] = '{16'hFF0F , 16'h1DFF};
-
-/*localparam IO_DEVICE1_LSB = 16'hFF00;
-localparam IO_DEVICE1_MSB = 16'hFF0F;      
-
-localparam IO_DEVICE2_LSB = 16'h1C00; 
-localparam IO_DEVICE2_MSB = 16'h1DFF;      
-*/
+localparam bit[16:0] IO_LSB[1:0] = '{16'hFF00 , 16'h1C00};   //65280,7168
+localparam bit[16:0] IO_MSB[1:0] = '{16'hFF0F , 16'h1DFF};   //65295,7679
 
 Intel8088 P(CLK, MNMX, TEST, RESET, READY, NMI, INTR, HOLD, AD, A, HLDA, IOM, WR, RD, SSO, INTA, ALE, DTR, DEN);
-FSM m1 (CLK,RESET,ALE,RD,WR,Address,Data , OE,rw , ld_Addr,ld_data );
-
-/*
-datapath #(MEM_LSB[0] , MEM_MSB[0]) m2 (CLK,Address,ld_Addr,rw,OE,cs[0],ld_data,Data);
-datapath #(MEM_LSB[1] , MEM_MSB[1]) m3 (CLK,Address,ld_Addr,rw,OE,cs[1],ld_data,Data);
-datapath #(IO_LSB[0] , IO_MSB[0]) m4 (CLK,Address,ld_Addr,rw,OE,cs[2],ld_data,Data);
-datapath #(IO_LSB[1] , IO_MSB[1]) m5 (CLK,Address,ld_Addr,rw,OE,cs[3],ld_data,Data);
-*/
+FSM #(MEM_LSB[0] , MEM_MSB[0],0 , "MEM1.txt" ) mem0 (CLK,RESET,ALE,IOM,CS[0],RD,WR,Address,Data);
+FSM #(MEM_LSB[1] , MEM_MSB[1],0 , "MEM2.txt" ) mem1 (CLK,RESET,ALE,IOM,CS[1],RD,WR,Address,Data);
+FSM #(IO_LSB[0] , IO_MSB[0],1 , "IO2.txt" ) IO0 (CLK,RESET,ALE,IOM,CS[2],RD,WR,Address,Data);
+FSM #(IO_LSB[1] , IO_MSB[1],1 , "IO1.txt") IO1 (CLK,RESET,ALE,IOM,CS[3],RD,WR,Address,Data);
 
 
-generate 
-	for(i=0;i<2;i++)
-	begin 
-		datapath #(MEM_LSB[i] , MEM_MSB[i]) mem (CLK,Address,ld_Addr,rw,OE,cs[i],ld_data,Data);	
-	end
-endgenerate 
-
-generate 
-	for(j=0;j<2;j++)
-	begin 
-		datapath #(IO_LSB[j] , IO_MSB[j]) IO (CLK,Address,ld_Addr,rw,OE,cs[j+2],ld_data,Data);	
-	end
-endgenerate 
-
-
-always@(posedge ALE )
-begin
- if(IOM == 0 & ALE)
-	cs = {1'b0 , 1'b0 , A[19], ~A[19]};
- else if (IOM == 1'b1 & ALE )
-    cs = {A[15], ~A[15],1'b0 , 1'b0 };
- else 
-  cs = '0;
-end
-
-/*
-always@(posedge ALE)
-begin 
-	if(IOM == 0)	cs = Address[19];
-	else cs = Address[15];
-end 
-*/
+assign CS[0] = ~Address[19];
+assign CS[1] = Address[19];
+assign CS[2] = Address[15:8] == 8'b0001_1100;
+assign CS[3] = Address[15:4] == 12'b1111_1111_0000;
 
 // 8282 Latch to latch bus address
 always_latch
